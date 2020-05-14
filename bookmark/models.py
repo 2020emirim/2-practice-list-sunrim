9460f42e32df35ab5af8e25d9db88b5e7de27f7b
mokmalla <shl1933@naver.com>
2020-05-14 09:21:07 +0900

    models Bookmark __str__(self)

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=20)
-    url = models.URLField('Site URL') # 이름 붙이기
+    url = models.URLField('Site URL')  # 이름 붙이기
+
+    def __str__(self):
+        return "제목 : " + self.site_name + ", URL : " + self.url
