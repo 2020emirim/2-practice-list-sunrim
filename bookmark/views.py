6e22a5c9769fcf2be50042c7ee43be3709524e4e
mokmalla <shl1933@naver.com>
2020-05-21 10:34:52 +0900

    views BookmarkDetailView

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 
 from bookmark.models import Bookmark
 
 
-class BookmarkList(ListView):   # bookmark_list.html
+class BookmarkList(ListView):  # bookmark_list.html
     model = Bookmark
 
-class BookmarkCreateView(CreateView):   #bookmark_form.html
+
+class BookmarkCreateView(CreateView):  # bookmark_form.html
     model = Bookmark
-    fields = ['site_name', 'url']       #<from>태그가 들어가는 곳에 넣어줌
-    template_name_suffix = '_create'    #bookmark_create.html
+    fields = ['site_name', 'url']  # <from>태그가 들어가는 곳에 넣어줌
+    template_name_suffix = '_create'  # bookmark_create.html
     success_url = reverse_lazy('bookmark:list')
+
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
