97f24af21093b60386ba8118d7ba06946925c30e
mokmalla <shl1933@naver.com>
2020-05-22 15:42:08 +0900

    views BookmarkDeleteViews

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from bookmark.models import Bookmark
 
@@ -23,3 +23,8 @@ class BookmarkUpdateView(UpdateView):
     model = Bookmark
     fields = ['site_name', 'url'] # <form>가 있을 때 쓴다
     template_name_suffix = '_update'    #bookmark_update.html
+    success_url = reverse_lazy('bookmark:list')
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('bookmark:list')
